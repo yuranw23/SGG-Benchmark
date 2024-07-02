ea308d5950779500b8cbb7b6cda03d956fcfcd4d
Yuran Wang <yuranw3@illinois.edu>
2024-07-02 18:42:33 -0400

    add PrototypeEmbeddingNetwork

--- sgg_benchmark/data/datasets/visual_genome.py
+++ sgg_benchmark/data/datasets/visual_genome.py
@@ -71,7 +71,7 @@ class VGDataset(torch.utils.data.Dataset):
                 filter_non_overlap=self.filter_non_overlap,
             )
             self.filenames, self.img_info = load_image_filenames(img_dir, image_file) # length equals to split_mask
-            print(f'filenames length = {len(self.filenames)} || filename[0] = self.filenames[0]')
+            print(f'filenames length = {len(self.filenames)} || filename[0] = {self.filenames[0]}')
 
             self.filenames = [self.filenames[i] for i in np.where(self.split_mask)[0]]
             self.img_info = [self.img_info[i] for i in np.where(self.split_mask)[0]]
