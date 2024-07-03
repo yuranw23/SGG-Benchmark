c2a427c78b90c06c892a8cb4c552d254b2f39ae3
Yuran Wang <yuranw3@illinois.edu>
2024-07-03 16:08:57 -0400

    acc metrics

--- acc_metrics.py
+++ acc_metrics.py
@@ -16,20 +16,36 @@ if __name__ == '__main__':
     # metrics calculation
     correct_obj = 0
     total_obj = 0
+    correct_pred = 0
+    total_pred = 0
 
-    for prediction in predictions:
+    for i in range(len(predictions)):
+        groundtruth = groundtruths[i]
+        prediction = predictions[i]
+        
+        # obj metric
         labels = prediction.get_field('labels')
         predict_logits = prediction.get_field('predict_logits')
         pred_labels = torch.argmax(predict_logits, axis=1)
-        
-        # pred_rel_labels = prediction.get_field('pred_rel_labels') # [29, 29] relation 
-        # rel_pair_idxs = prediction.get_field('rel_pair_idxs') # [[2,3], [2,4]] every pair of objects
-        # labels, predict_logits, pred_labels, pred_scores, rel_pair_idxs, pred_rel_scores, pred_rel_labels
-        
-        # pred_rel_scores = prediction.get_field('pred_rel_scores') # (20 combination, 51: 50 rels + none)
-        
-        # obj metric
         correct_obj += sum(labels == pred_labels).item()
         total_obj += len(labels)
-    
+
+        # predicate metric
+        gt_rels = groundtruth.get_field('relation_tuple')
+        total_pred += len(gt_rels)
+        pred_rel_pair = prediction.get_field('rel_pair_idxs').tolist()
+        pred_rel_label = prediction.get_field('pred_rel_scores')
+        pred_rel_label[:,0] = 0
+        pred_rel_score, pred_rel_label = pred_rel_label.max(-1)
+
+        for gt_rel in gt_rels:
+            obj_pair = gt_rel[:2].tolist()
+            gt_pred = gt_rel[-1].item()
+            pair_idx = pred_rel_pair.index(obj_pair)
+            pred_rel = pred_rel_label[pair_idx].item()
+            if pred_rel == gt_pred:
+                correct_pred += 1        
+        
+    print(f'correct obj = {correct_obj} || total obj = {total_obj} || correct pred = {correct_pred} || total pred = {total_pred}')
     print(f'obj acc = {correct_obj / total_obj}')
+    print(f'pred acc = {correct_pred / total_pred}')
