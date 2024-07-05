c1eab72b19c96db1c62c0431fdc3a19ba2a109d9
Yuran Wang <yuranw3@illinois.edu>
2024-07-04 22:41:28 -0400

    RU-Net

--- sgg_benchmark/modeling/roi_heads/relation_head/roi_relation_predictors.py
+++ sgg_benchmark/modeling/roi_heads/relation_head/roi_relation_predictors.py
@@ -5,7 +5,7 @@ from .predictors.GCL_predictors import *
 from .predictors.bayesian_predictors import *
 from .predictors.regularized_predictors import *
 from .predictors.penet_simple import *
-# from .predictors.PrototypeEmbeddingNetwork import *
+from .predictors.RUNet_predictor import *
 
 def make_roi_relation_predictor(cfg, in_channels):
     func = registry.ROI_RELATION_PREDICTOR[cfg.MODEL.ROI_RELATION_HEAD.PREDICTOR]
